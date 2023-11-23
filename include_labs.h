4b13aef69f590f5bd83f2fd97143c56ae3d6d959
DVMN <zon9supadm@gmail.com>
2023-11-23 22:27:32 +0700

    Прошлые наработки

---
+++ include_labs.h
@@ -0,0 +1,14 @@
+#pragma once
+void Labs(int labaratornaya, int variant);
+void Lab1(int variant);
+void Lab2(int variant);
+void Lab3(int variant);
+void Lab4(int variant);
+void Lab5(int variant);
+void Lab6(int variant);
+void Lab7(int variant);
+void Lab8(int variant);
+void Lab9(int variant);
+void Lab10(int variant);
+void Lab11(int variant);
+void Lab12(int variant);
