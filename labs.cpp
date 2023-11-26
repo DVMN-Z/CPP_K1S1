47139155160a41e5bcf17ae7269dd0e2473754a4
DVMN <zon9supadm@gmail.com>
2023-11-26 12:02:42 +0700

    Будущее место для лабараторных

---
+++ labs.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void lab2()
+{
+	cout << "test1";
+}
+
+
+
+void lab3()
+{
+	cout << "test12";
+}
+
+
+
+void lab4()
+{
+
+}
+
+
+
+void lab5()
+{
+
+}
+
+
+
+void lab6()
+{
+
+}
+
+
+
+void lab7()
+{
+
+}
+
+
+
+void lab8()
+{
+
+}
+
+
+
+void lab9()
+{
+
+}
+
+
+
+void lab10()
+{
+
+}
+
+
+
+void lab11()
+{
+
+}
+
+
+
+void lab12()
+{
+
+}
+
+void select_Lab(int lab)
+{
+	cout << "\n\n" << endl;
+	if (lab == 2) { lab2(); }
+	else if (lab == 3) { lab3(); }
+	else if (lab == 4) { lab4(); }
+	else if (lab == 5) { lab5(); }
+	else if (lab == 6) { lab6(); }
+	else if (lab == 7) { lab7(); }
+	else if (lab == 8) { lab8(); }
+	else if (lab == 9) { lab9(); }
+	else if (lab == 10) { lab10(); }
+	else if (lab == 11) { lab11(); }
+	else if (lab == 12) { lab12(); }
+}
